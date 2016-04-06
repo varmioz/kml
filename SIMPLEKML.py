277184521465dacb20024c6f8060df77d5a82e5b
Dmitriy Gubarev <dgubarev@outlook.com>
2016-04-06 23:15:52 +0300

    dictionary instead of tuple

--- SIMPLEKML.py
+++ SIMPLEKML.py
@@ -31,21 +31,42 @@ def dist_calc(lat2,lon2,lat1,lon1):
     return km
 
 file = "Cell.csv"
-with open(file, 'rb') as f:
-    data=[tuple(line) for line in csv.reader(f)]
-    data.remove(data[0])
-
+with open(file, 'rb') as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = [row for row in reader]
+    
 # Create an instance of Kml
 kml = simplekml.Kml(open=1)
 
 # Create a point for each city. The points' properties are assigned after the point is created
-for cell,lon,lat,direction,beam,UARFCN,SC,RNC,cellid,LAC in data:
-    pnt = kml.newpoint()
-    pnt.name = cell
-    pnt.description = "RNC - " + RNC + "LAC - " + LAC
-    pnt.coords = [(lon, lat)]
-    pnt.style.labelstyle.scale = 0.5
-    pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/pal2/icon26.png'
+fol = kml.newfolder(name='Sites')
+fol1 = fol.newfolder(name='Inside Folder')
+
+sites = {}
+for row in data:
+    pnt = fol1.newpoint()
+    pnt.name = row['Cell']
+    pnt.description = "LAC - " + row['LAC']
+    pnt.coords = [(row['Lon'], row['Lat'])]
+
+
+#polygon
+pol = kml.newpolygon(name="Atrium Garden",
+             outerboundaryis=[(18.43348,-33.98985),(18.43387,-33.99004),(18.43410,-33.98972),
+                              (18.43371,-33.98952),(18.43348,-33.98985)],
+             innerboundaryis=[(18.43360,-33.98982),(18.43386,-33.98995),(18.43401,-33.98974),
+                              (18.43376,-33.98962),(18.43360,-33.98982)])
+pol.style.linestyle.color = simplekml.Color.red
+pol.style.linestyle.width = 5
+pol.style.polystyle.color = simplekml.Color.changealphaint(100, simplekml.Color.green)
+
+
+#LineString
+ls = kml.newlinestring(name='A LineString')
+ls.coords = [(18.333868,-34.038274,10.0), (18.370618,-34.034421,10.0)]
+ls.altitudemode = simplekml.AltitudeMode.relativetoground
+ls.visibility = 0
+ls.description = "Short description"
 
 # Save the KML
 print len(data)
