d2b293db40e82922dc96f9751cd70d830450ddb3
Dmitriy Gubarev <dgubarev@outlook.com>
2016-04-08 00:00:43 +0300

    working color import

--- SIMPLEKML.py
+++ SIMPLEKML.py
@@ -1,7 +1,8 @@
 
-import math
+import math, random
 import simplekml
 import csv
+from webcolors import name_to_hex as nth
 
 R = 6378.1 #Radius of the Earth in km
 
@@ -30,7 +31,7 @@ def dist_calc(lat2,lon2,lat1,lon1):
     km = R * (2 * math.asin(math.sqrt(a)))
     return km
 
-file = "Cell.csv"
+file = "Sites.csv"
 with open(file, 'rb') as csvfile:
     reader = csv.DictReader(csvfile)
     data = [row for row in reader]
@@ -39,17 +40,35 @@ with open(file, 'rb') as csvfile:
 kml = simplekml.Kml(open=1)
 
 # Create a point for each city. The points' properties are assigned after the point is created
-fol = kml.newfolder(name='Sites')
-fol1 = fol.newfolder(name='Inside Folder')
 
-sites = {}
+fold = None
+subfold = None
+
 for row in data:
+    if fold <> row['Folder']:
+        fold = row['Folder']
+        fol = kml.newfolder(name = fold)
+        subfold = row['Subfolder']
+        fol1 = fol.newfolder(name=subfold)
+        if row['Color'] == 'random': color = "ff%06x" % random.randint(0, 0xFFFFFF)
+        else: color = 'ff'+nth(row['Color'])[1:]
+        
+    if subfold <> row['Subfolder']:
+        subfold = row['Subfolder']
+        fol1 = fol.newfolder(name=subfold)
+        if row['Color'] == 'random': color = "ff%06x" % random.randint(0, 0xFFFFFF)
+        else: color = 'ff'+nth(row['Color'])[1:]      
+
     pnt = fol1.newpoint()
-    pnt.name = row['Cell']
-    pnt.description = "LAC - " + row['LAC']
+    pnt.name = row['Site']
+    pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/donut.png'
+    pnt.style.iconstyle.scale = 0.8
+    pnt.style.iconstyle.color = color
+    pnt.style.labelstyle.scale = 0.6
+    pnt.description = "Date - " + row['Comment']
     pnt.coords = [(row['Lon'], row['Lat'])]
 
-
+'''
 #polygon
 pol = kml.newpolygon(name="Atrium Garden",
              outerboundaryis=[(18.43348,-33.98985),(18.43387,-33.99004),(18.43410,-33.98972),
@@ -67,7 +86,7 @@ ls.coords = [(18.333868,-34.038274,10.0), (18.370618,-34.034421,10.0)]
 ls.altitudemode = simplekml.AltitudeMode.relativetoground
 ls.visibility = 0
 ls.description = "Short description"
-
+'''
 # Save the KML
 print len(data)
 kml.save("T00 Point.kml")
